13b811a5f4f83923e5424b790389fe16cc125ede
WDGustavo <WDGustavo@github.com>
2021-03-07 21:57:37 -0300

    tarefavector3

---
+++ Exercicios/Assets/Patrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public List<Transform> points = new List<Transform>();
+    Transform finalpoint;
+    int point = 0;
+    bool reverse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalpoint = points[point];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, finalpoint.position) > 0.2f)
+            transform.position += (finalpoint.position - transform.position).normalized * 8f * Time.deltaTime;
+        else
+            ChangePatrolPoint();
+    }
+    void ChangePatrolPoint()
+    {
+        if (!reverse)
+            point++;
+        else
+            point--;
+
+        if (point >= points.Count)
+        {
+            point = (points.Count - 1);
+            reverse = true;
+        }
+        else if (point < 0)
+        {
+            point = 0;
+            reverse = false;
+        }
+
+        finalpoint = points[point];
+    }
+}
---
+++ Exercicios/Assets/Uniforme.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uniforme : MonoBehaviour
+{
+    public Transform finalpoint;
+    public float velocidade =5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (finalpoint.position - transform.position).normalized*velocidade*Time.deltaTime;
+    }
+}
